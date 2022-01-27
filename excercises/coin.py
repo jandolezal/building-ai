f3cd095e0835def5850074342c76fb23346f7e98
Jan Doležal <jandolezal@email.cz>
2022-01-27 07:13:14 +0100

    Add advanced part for seventh excercise

--- excercises/coin.py
+++ excercises/coin.py
@@ -31,3 +31,34 @@ def count11(seq):
 print(count11([0, 0, 1, 1, 1, 0])) # this should print 2
 
 
+# Advanced
+
+"""
+Write a program that generates 10000 random zeros and ones where the probability of one is p1 and the
+probability of zero is 1-p1 (hint: np.random.choice([0,1], p=[1-p1, p1], size=10000)),
+counts the number of occurrences of 5 consecutive ones ("11111") in the sequence, and outputs this number
+as a return value.
+Check that for p1 = 2/3, the count is close to 10000 x (2/3)^5 ≈ 1316.9.
+"""
+
+import numpy as np
+
+def generate(p1):
+    """Generates 10000 random zeros and ones where the probability of one is p1"""
+    seq = np.random.choice([0, 1], p=[1-p1, p1], size=10000)
+    return seq
+
+def count(seq):
+    """Returns the number of occurrences of 11111 in the sequence"""
+    count = 0
+    for i in range(len(seq)-4):
+        if all(seq[i:i+5]):
+            count += 1
+    return count
+
+
+def main(p1):
+    seq = generate(p1)
+    return count(seq)
+
+print(main(2/3))
