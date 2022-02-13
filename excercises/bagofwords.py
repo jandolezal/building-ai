245181a0dd2c6856f535bd81bd3aade1e5a836a1
Jan Doležal <jandolezal@email.cz>
2022-02-13 14:31:45 +0100

    Add exercise 17 bag of words

---
+++ excercises/bagofwords.py
@@ -0,0 +1,57 @@
+"""
+Exercise 17: Bag of words
+
+https://buildingai.elementsofai.com/Machine-Learning/working-with-text
+"""
+
+
+# Intermediate
+
+# this data here is the bag of words representation of This Little Piggy
+data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
+
+def distance(row1, row2):
+    return sum(abs(count1 - count2) for count1, count2 in zip(row1, row2))
+
+def all_pairs(data):
+    # this calls the distance function for all the two-row combinations in the data
+    # you do not need to change this
+    dist = [[distance(sent1, sent2) for sent1 in data] for sent2 in data]
+    print(dist)
+
+all_pairs(data)
+
+
+
+# Advanced
+
+import numpy as np
+
+data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
+
+
+def distance(row1, row2):
+    return sum(abs(count1 - count2) for count1, count2 in zip(row1, row2))
+
+
+def find_nearest_pair(data):
+    N = len(data)
+    dist = np.empty((N, N), dtype=np.float)
+    for i, row1 in enumerate(data):
+        for j, row2 in enumerate(data):
+            if i == j:
+                dist[i, j] = np.Inf
+            else:
+                dist[i, j] = distance(row1, row2)
+
+    print(np.unravel_index(np.argmin(dist), dist.shape))
+
+find_nearest_pair(data)
